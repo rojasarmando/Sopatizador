edab9def2d466604c1877930bc0cda42d55a79f4
Armando Rojas <90494082+rojasarmando@users.noreply.github.com>
2022-06-04 20:49:34 -0400

    remove whitespace

--- js/kajataca.jquery.js
+++ js/kajataca.jquery.js
@@ -1,108 +1,98 @@
- /*
+/*
 
 	Kajataca.js
- 
 
-	Autor = Armando Rojas - UNEFA 2017 
+
+	Autor = Armando Rojas - UNEFA 2017
 
 */
-String.prototype.trim = function() { return this.replace(/^\s+|\s+$/g, "")}
+String.prototype.trim = function () {
+  return this.replace(/^\s+|\s+$/g, "");
+};
+(function ($) {
+  $.fn.extend({
+    /*Validacion de Formularios*/
 
+    // no permite copiar y pegar en la caja de texto
+    noCopiar: function () {
+      return this.each(function () {
+        $(this).bind("cut copy paste", function (e) {
+          e.preventDefault();
+          return false;
+        });
+      });
+    },
 
+    // transformar letras a mayuscula
+    mayuscula: function () {
+      return this.each(function () {
+        $(this).keyup(function (event) {
+          $(this).val($(this).val().toUpperCase());
+        });
+      });
+    },
 
-;(function($){
+    // validar entrada por teclado
+    validCampo: function (b) {
+      $(this).on({
+        keypress: function (a) {
+          var c = a.which,
+            d = a.keyCode,
+            e = String.fromCharCode(c).toLowerCase(),
+            f = b;
+          ((-1 != f.indexOf(e) ||
+            9 == d ||
+            (37 != c && 37 == d) ||
+            (39 == d && 39 != c) ||
+            8 == d ||
+            (46 == d && 46 != c)) &&
+            161 != c) ||
+            a.preventDefault();
+        },
+      });
+    },
 
-	$.fn.extend({
-
-
-
-		/*Validacion de Formularios*/
-		
-		// no permite copiar y pegar en la caja de texto
-		noCopiar: function(){
-
-			return this.each(function(){
-				$(this).bind("cut copy paste", function(e){
-				e.preventDefault();
-				return false;
-				});
-			});
-		},
-
-		// transformar letras a mayuscula 
-		mayuscula: function(){
-
-			return this.each(function() {
-				$(this).keyup(function(event) {
-   					$(this).val($(this).val().toUpperCase());
-  				 });
-			});
-		},
-
-		// validar entrada por teclado
-		validCampo: function(b){
-			$(this).on(
-				{
-					keypress:function(a){
-					var c=a.which,
-					d=a.keyCode,
-					e=String.fromCharCode(c).toLowerCase(),
-					f=b;
-					(-1!=f.indexOf(e)||9==d||37!=c&&37==d||39==d&&39!=c||8==d||46==d&&46!=c)&&161!=c||a.preventDefault();
-					
-					}
-			});
-		},
-
-		// validar maximo de cadena 	
-
-	
-
-	});
+    // validar maximo de cadena
+  });
 })(jQuery);
-
 
 /*	Libreria para Mostrar Hora y Fecha */
 
 const miTiempo = {
+  hora_local: function () {
+    var data = new Date(); // obtener la hora y fecha del sistema
+    var hora = data.getHours(); // obtener hora
+    var minutos = data.getMinutes(); // obtener minutos
+    var segundos = data.getSeconds();
 
-	hora_local : function(){
+    var regimen = "am";
 
-		var data = new Date(); // obtener la hora y fecha del sistema
-		var hora = data.getHours(); // obtener hora
-		var minutos = data.getMinutes(); // obtener minutos 
-		var segundos = data.getSeconds(); 
+    /* Agregar un 0 al izquiera si es necesario*/
+    if (hora < 10) hora = "0" + hora.toString();
+    if (minutos < 10) minutos = "0" + minutos.toString();
+    if (segundos < 10) segundos = "0" + segundos.toString();
 
-		var regimen = "am"; 
-		
-		/* Agregar un 0 al izquiera si es necesario*/
-		if(hora < 10) hora = "0"+hora.toString();
-		if(minutos < 10) minutos = "0"+minutos.toString();
-		if(segundos < 10) segundos = "0"+segundos.toString();
+    /*Cambiar Regimen y establecer formato 12h*/
+    if (hora > 12) {
+      regimen = "pm";
+      hora -= 12;
+    }
 
-		/*Cambiar Regimen y establecer formato 12h*/
-		if(hora > 12) {
-			regimen = "pm"; 
-			hora -= 12; 
-		}
-
-			/*Devuelve Formato*/
-		return hora+":"+minutos+":"+segundos+regimen;
-	}
+    /*Devuelve Formato*/
+    return hora + ":" + minutos + ":" + segundos + regimen;
+  },
 };
 
 /*
-	Expresiones Regulares 
+	Expresiones Regulares
  */
 
-function soloLetras(){
-
-	return "{ABCDEFGHIJKMNLOPQRSTUVWXYZÑabcdefghijklmnopqrstuvwxyzñ}"
+function soloLetras() {
+  return "{ABCDEFGHIJKMNLOPQRSTUVWXYZÑabcdefghijklmnopqrstuvwxyzñ}";
 }
 
-function soloNumeros(){
-
-	return "{1234567890}"
+function soloNumeros() {
+  return "{1234567890}";
 }
 
 /* Invocar ayuda */
--- js/validar.js
+++ js/validar.js
@@ -1,56 +1,42 @@
-$(document).ready(function() {
-	
-	$('.sopa').click(()=>{ 
-		mensajeBienvenida() 
-	});
+$(document).ready(function () {
+  $(".sopa").click(() => {
+    mensajeBienvenida();
+  });
 
-	mensajeBienvenida() 
+  mensajeBienvenida();
 
-	$("input[type=text]").mayuscula()
-	$("input[type=text]").validCampo(soloLetras())
-	$("input[type=number]").validCampo(soloNumeros())
-	$("input").noCopiar()
-
-
-	
+  $("input[type=text]").mayuscula();
+  $("input[type=text]").validCampo(soloLetras());
+  $("input[type=number]").validCampo(soloNumeros());
+  $("input").noCopiar();
 });
 
-
-
-function mensajeError(texto){
-	swal({
-	  title: '¡Oh no, ah ocurrido un Error!',
-	  type: 'error',
-	  html: $('<div>')
-	    .addClass('some-class')
-	    .text(texto),
-	  animation: true,
-
-	})
+function mensajeError(texto) {
+  swal({
+    title: "¡Oh no, ah ocurrido un Error!",
+    type: "error",
+    html: $("<div>").addClass("some-class").text(texto),
+    animation: true,
+  });
 }
 
-function mensajeOk(texto){
-	swal({
-	  title: '¡Okey!',
-	  type: 'success',
-	  html: $('<div>')
-	    .addClass('some-class')
-	    .text(texto),
-	  animation: true,
-	})
+function mensajeOk(texto) {
+  swal({
+    title: "¡Okey!",
+    type: "success",
+    html: $("<div>").addClass("some-class").text(texto),
+    animation: true,
+  });
 }
 
-function mensajeBienvenida(texto){
-		swal({
-	  title: '¡Bienvenido al Sopatizador!',
-	  text: 'Una App pensada para tí, para generar sopa de letras facíl y rapído, olvidate de esos tediosos cuadritos de excel de una vez por todas by: Armando Rojas',
-	  imageUrl: 'img/icono.ico',
-	  imageWidth: 200,
-	  imageHeight: 200,
-	  imageAlt: 'Custom image',
-	  animation: true
-	})
+function mensajeBienvenida(texto) {
+  swal({
+    title: "¡Bienvenido al Sopatizador!",
+    text: "Una App pensada para tí, para generar sopa de letras facíl y rapído, olvidate de esos tediosos cuadritos de excel de una vez por todas by: Armando Rojas",
+    imageUrl: "img/icono.ico",
+    imageWidth: 200,
+    imageHeight: 200,
+    imageAlt: "Custom image",
+    animation: true,
+  });
 }
-
-
-
