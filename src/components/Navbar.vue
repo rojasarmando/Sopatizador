fd55ce4b74c0dc26c28ad96b02b1d55e135ba194
Armando Rojas <rojasarmando2017@gmail.com>
2023-02-19 01:17:07 -0400

    upgrade

---
+++ src/components/Navbar.vue
@@ -0,0 +1,7 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-ligth bg-dark">
+    <div class="container">
+      <h3 class="soup text-center m-auto">Sopa de Letras</h3>
+    </div>
+  </nav>
+</template>
